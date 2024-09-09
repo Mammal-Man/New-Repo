cbdc6db158760e7f7b4002aa0cdc1e0f01a2a93b
Mammal-Man <139150865+Mammal-Man@users.noreply.github.com>
2024-09-09 12:58:29 -0700

    Basic Forward Movement

--- chicken/Assets/Scripts/PlayerControl.cs
+++ chicken/Assets/Scripts/PlayerControl.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
+    Rigidbody myRB;
+
+    public float MoveSpeed = 10;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        myRB = GetComponent<Rigidbody>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.W))
+        {
+            myRB.AddForce(transform.forward * MoveSpeed) ;
+
+        }
     }
 }
