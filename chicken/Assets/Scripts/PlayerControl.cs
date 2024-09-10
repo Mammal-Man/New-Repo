6bde90ecbee657324e932c52419d5d92b7a3e266
Mammal-Man <139150865+Mammal-Man@users.noreply.github.com>
2024-09-10 13:35:50 -0700

    SprintTog

--- chicken/Assets/Scripts/PlayerControl.cs
+++ chicken/Assets/Scripts/PlayerControl.cs
@@ -12,10 +12,14 @@ public class PlayerControl : MonoBehaviour
 
     public bool sprinting = false;
 
+    [Header("Movement Settings")]
     public float MoveSpeed = 10;
     public float sprintMulti = 2.5f;
     public float JumpHeight = 5;
 
+
+    [Header("User Settings")]
+    public bool sprintTogOpt = false;
     public float mouseSens = 2;
     public float xSens = 2;
     public float ySens = 2;
@@ -25,7 +29,7 @@ public class PlayerControl : MonoBehaviour
 
     // Start is called before the first frame update
     void Start()
-    { 
+    {
         // Initialize components
         myRB = GetComponent<Rigidbody>();
         playerCam = transform.GetChild(0).GetComponent<Camera>();
@@ -47,28 +51,36 @@ public class PlayerControl : MonoBehaviour
 
         //Neck Brace
         camRot.y = Mathf.Clamp(camRot.y, -camRotLim, camRotLim);
-       
+
         //Movement variables
         Vector3 temp = myRB.velocity;
+        float vertMove = Input.GetAxisRaw("Vertical");
 
         //Run for your life
-        if (Input.GetKey(KeyCode.LeftControl))
-        {sprinting = true;}
-        
+        if (!sprintTogOpt)
+        {
+            if (Input.GetKey(KeyCode.LeftControl))
+            { sprinting = true; }
+            if (Input.GetKeyUp(KeyCode.LeftControl))
+            { sprinting = false; }
+        }
+
+        if (sprintTogOpt)
+        {
+            if (Input.GetKey(KeyCode.LeftControl) && vertMove > 0)
+            { sprinting = true; }
+        }
         if (!sprinting)
-        {temp.x = Input.GetAxisRaw("Vertical") * MoveSpeed;}
+        { temp.x = vertMove * MoveSpeed; }
 
         if (sprinting)
-        {temp.x = Input.GetAxisRaw("Vertical") * MoveSpeed *sprintMulti;}
-
-        if (Input.GetKeyUp(KeyCode.LeftControl))
-        {sprinting = false;}
+        { temp.x = vertMove * MoveSpeed * sprintMulti; }
 
         temp.z = Input.GetAxisRaw("Horizontal") * MoveSpeed;
 
         //Jump for joy
         if (Input.GetKeyDown(KeyCode.Space) && Physics.Raycast(transform.position, -transform.up, groundDetecDist))
-        {temp.y = JumpHeight;}
+        { temp.y = JumpHeight; }
 
         // Use the Force Luke
         myRB.velocity = (temp.x * transform.forward) + (temp.z * transform.right) + (temp.y * transform.up);
