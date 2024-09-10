3f1bab18b95b77cdf371919ea792fbee714d9bb5
Mammal-Man <139150865+Mammal-Man@users.noreply.github.com>
2024-09-10 13:08:08 -0700

    Run for your life!

--- chicken/Assets/Scripts/PlayerControl.cs
+++ chicken/Assets/Scripts/PlayerControl.cs
@@ -10,6 +10,8 @@ public class PlayerControl : MonoBehaviour
 
     Vector2 camRot;
 
+    public bool sprinting = false;
+
     public float MoveSpeed = 10;
     public float sprintMulti 2.5f;
     public float JumpHeight = 5;
@@ -49,20 +51,24 @@ public class PlayerControl : MonoBehaviour
         //Movement variables
         Vector3 temp = myRB.velocity;
 
-        temp.x = Input.GetAxisRaw("Vertical") * MoveSpeed;
-        temp.z = Input.GetAxisRaw("Horizontal") * MoveSpeed;
-
         //Run for your life
         if (Input.GetKey(KeyCode.LeftControl))
-        {
-            temp.x *= sprintMulti;
-        }
+        {sprinting = true;}
+        
+        if (!sprinting)
+        {temp.x = Input.GetAxisRaw("Vertical") * MoveSpeed;}
+
+        if (sprinting)
+        {temp.x = Input.GetAxisRaw("Vertical") * MoveSpeed *sprintMulti;}
+
+        if(Input.GetKeyUp(KeyCode.LeftControl))
+
+        temp.z = Input.GetAxisRaw("Horizontal") * MoveSpeed;
 
         //Jump for joy
         if (Input.GetKeyDown(KeyCode.Space) && Physics.Raycast(transform.position, -transform.up, groundDetecDist))
-        {
-            temp.y = JumpHeight;
-        }
+        {temp.y = JumpHeight;}
+
         // Use the Force Luke
         myRB.velocity = (temp.x * transform.forward) + (temp.z * transform.right) + (temp.y * transform.up);
     }
