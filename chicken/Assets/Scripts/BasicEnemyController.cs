d628a4eaab822d23bab2db76f0c290e59b095ac1
Mammal-Man <139150865+Mammal-Man@users.noreply.github.com>
2024-09-24 12:21:53 -0700

    I know the location at which you reside

--- chicken/Assets/Scripts/BasicEnemyController.cs
+++ chicken/Assets/Scripts/BasicEnemyController.cs
@@ -1,28 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
-public class EnemyAI : MonoBehaviour
+public class BasicEnemyController : MonoBehaviour
 {
+    
+    public PlayerControl player;
+    public NavMeshAgent agent;
 
+    [Header("Enemy Stats")]
     public int health = 3;
     public int maxHealth = 3;
+    public int damageGiven = 5;
+    public int damageRecieved = 1;
+    public float pushBackForce = 10000;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.Find("Player").GetComponent<PlayerControl>();
+        agent = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        //Kill me
         if (health <= 0)
             Destroy(gameObject);
+
+        //Find him
+        agent.destination = player.transform.position;
     }
 
     private void OnCollisionEnter(Collision collision)
     {
+        //Ive been shot?
         if (collision.gameObject.tag == "Shot")
         {
             Destroy(collision.gameObject);
