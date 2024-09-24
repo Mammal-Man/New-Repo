e6d8589a038a37bd8cbd8700571a5c83df87594a
Mammal-Man <139150865+Mammal-Man@users.noreply.github.com>
2024-09-24 11:44:39 -0700

    Catergorize

--- chicken/Assets/Scripts/EnemyAI.cs
+++ chicken/Assets/Scripts/EnemyAI.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class EnemyAI : MonoBehaviour
 {
+
+    public int health = 3;
+    public int maxHealth = 3;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,16 @@ public class EnemyAI : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (health <= 0)
+            Destroy(gameObject);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Shot")
+        {
+            Destroy(collision.gameObject);
+            health--;
+        }
     }
 }
