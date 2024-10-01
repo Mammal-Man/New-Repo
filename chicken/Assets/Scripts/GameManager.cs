195edd504a24d0f0e879a29dd35fdb1cf152e391
Mammal-Man <139150865+Mammal-Man@users.noreply.github.com>
2024-10-01 12:02:52 -0700

    Game manager setup

---
+++ chicken/Assets/Scripts/GameManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+    public bool isPaused = false;
+
+    public GameObject pauseMenu;
+    public PlayerControl PlayerController;
+
+    public Image healthBar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
